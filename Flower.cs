6d3ffc1865ae4991b01aa39d27b5f759f2196e24
allahbabah <107516270+allahbabah@users.noreply.github.com>
2022-06-15 11:17:14 +0300

    test

---
+++ Flower.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kp3
+{
+    class Flower:Bud
+    {
+        public string Name;
+        public void AddFlower(string name,string color,bool bloom,int petals)
+        {
+            Name = name;
+            Color = color; 
+            Bloom = bloom;
+            Petals = petals;
+        }
+    }
+    class Bud:Petal
+    {
+        public string Color;
+        public bool Bloom;
+
+        public void Blooming()
+        {
+            if (Bloom)
+            {
+                Console.WriteLine("Квітка цвіте");
+            }
+            else
+            {
+                Bloom = true;
+                Console.WriteLine("Квітка зацвіла");
+            }
+        }
+
+        public void Fade()
+        {
+            if (Bloom)
+            {
+                Bloom = false;
+                Console.WriteLine("Квітка зів'яла");
+            }
+            else 
+            {
+                Console.WriteLine("Квітка уже зів'яла");
+            }
+        }
+
+        public void ColorBud()
+        {
+            if (!Bloom)
+            {
+                Console.WriteLine("Квітка не цвіте");
+            }
+            else
+            {
+                Console.WriteLine(Color);
+            }
+        }
+    }
+    class Petal
+    {
+        public int Petals;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Flower flower1 = new Flower();
+            flower1.AddFlower("Троянда", "Червоний", true, 104);
+            flower1.Fade();
+            flower1.Blooming();
+            flower1.ColorBud();
+        }
+    }
+}
